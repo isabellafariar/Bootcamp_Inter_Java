b39487a0328781caee0b7fefb852615dc5674b7d
isabellafariar <isabellaf@id.uff.br>
2021-02-09 18:46:20 -0300

    curso de java

---
+++ Aula Java/exemplo-com-gradle/src/main/java/com/onlyme/exemplo/com/gradle/ExemploComGradleApplication.java
@@ -0,0 +1,13 @@
+package com.onlyme.exemplo.com.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploComGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploComGradleApplication.class, args);
+	}
+
+}
---
+++ Aula Java/exemplo-com-gradle/src/test/java/com/onlyme/exemplo/com/gradle/ExemploComGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyme.exemplo.com.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploComGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
